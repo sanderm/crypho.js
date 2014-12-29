96a467889629edaf735ad2451874fc064e676941
Yiorgis Gozadinos <ggozad@crypho.com>
2014-12-29 12:31:18 +0100

    Test husher

---
+++ tests/specRunner.js
@@ -0,0 +1,29 @@
+require.config({
+    baseUrl: '.',
+
+    urlArgs: 'cb=' + Math.random(),
+
+    packages: [
+        {
+            name: 'crypho',
+            location: '..',
+        }
+    ],
+
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        underscore: '../bower_components/underscore/underscore',
+        backbone: '../bower_components/backbone/backbone',
+        sjcl: 'sjcl'
+    },
+
+    shim: {
+    }
+});
+
+
+require(['jquery', 'specs/husher_spec'], function ($) {
+    var jasmineEnv = jasmine.getEnv();
+
+    window.onload();
+});
