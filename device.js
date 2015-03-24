6f68fd5d4f8e9114421e73a4615d788b17e69ddf
Yiorgis Gozadinos <ggozad@crypho.com>
2015-03-24 10:40:41 +0100

    Add device module

---
+++ device.js
@@ -0,0 +1,32 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'XMPP',
+    ], function ($, _, Backbone, XMPP) {
+
+    var Devices = {};
+
+    Devices.Device = Backbone.Model.extend({
+        idAttribute: 'device_id'
+    });
+
+    Devices.Devices = Backbone.Collection.extend({
+
+        model: Devices.Device,
+
+        fetch: function(options) {
+            var self = this,
+                d = $.Deferred();
+            XMPP.connection.Crypho.getDevices()
+            .done(function (json) {
+                self.set(json);
+                d.resolve(self);
+            })
+            .fail(d.reject);
+            return d.promise();
+        }
+    });
+
+    return Devices;
+});
