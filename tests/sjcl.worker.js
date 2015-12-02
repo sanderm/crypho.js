2d964b8957b4d99a76f83d39bdb783ecd62f3254
Yiorgis Gozadinos <ggozad@crypho.com>
2015-12-02 14:42:15 +0100

    Format worker

--- tests/sjcl.worker.js
+++ tests/sjcl.worker.js
@@ -1,67 +1,52 @@
 /* global sjcl, self */
-
-importScripts('sjcl.js');
+importScripts('../../static/libs/sjcl.js');
 
 var encryptBinary = function (password, plaintext, params) {
-        params = params || {};
-        var j = sjcl.json,
-            p = j._add(j.defaults,
-            {iv: sjcl.random.randomWords(4,0)}),
-            tmp,
-            prp,
-            adata,
-            ct;
+    params = params || {};
+    var j = sjcl.json, p = j._add(j.defaults, {iv: sjcl.random.randomWords(4,0)}), tmp, prp, adata, ct;
+    j._add(p, params);
+    adata = p.adata;
+    if (typeof p.salt === "string") {
+        p.salt = sjcl.codec.base64.toBits(p.salt);
+    }
+    if (typeof p.iv === "string") {
+        p.iv = sjcl.codec.base64.toBits(p.iv);
+    }
 
-        j._add(p, params);
-        adata = p.adata;
+    if (!sjcl.mode[p.mode] ||
+        !sjcl.cipher[p.cipher] ||
+        (typeof password === "string" && p.iter <= 100) ||
+        (p.ts !== 64 && p.ts !== 96 && p.ts !== 128) ||
+        (p.ks !== 128 && p.ks !== 192 && p.ks !== 256) ||
+        (p.iv.length < 2 || p.iv.length > 4)) {
+        throw new sjcl.exception.invalid("json encrypt: invalid parameters");
+    }
 
-        if (typeof p.salt === "string") {
-            p.salt = sjcl.codec.base64.toBits(p.salt);
-        }
+    if (typeof password === "string") {
+        tmp = sjcl.misc.cachedPbkdf2(password, p);
+        password = tmp.key.slice(0,p.ks/32);
+        p.salt = tmp.salt;
+    }
 
-        if (typeof p.iv === "string") {
-            p.iv = sjcl.codec.base64.toBits(p.iv);
-        }
+    plaintext = sjcl.codec.bytes.toBits(plaintext);
 
-        if (!sjcl.mode[p.mode] ||
-            !sjcl.cipher[p.cipher] ||
-            (typeof password === "string" && p.iter <= 100) ||
-            (p.ts !== 64 && p.ts !== 96 && p.ts !== 128) ||
-            (p.ks !== 128 && p.ks !== 192 && p.ks !== 256) ||
-            (p.iv.length < 2 || p.iv.length > 4)) {
-            throw new sjcl.exception.invalid("json encrypt: invalid parameters");
-        }
+    if (typeof adata === "string") {
+        adata = sjcl.codec.utf8String.toBits(adata);
+    }
 
-        if (typeof password === "string") {
-            tmp = sjcl.misc.cachedPbkdf2(password, p);
-            password = tmp.key.slice(0,p.ks/32);
-            p.salt = tmp.salt;
-        }
+    prp = new sjcl.cipher[p.cipher](password);
 
-        plaintext = sjcl.codec.bytes.toBits(plaintext);
+    /* return the json data */
+    j._add(p);
 
-        if (typeof adata === "string") {
-            adata = sjcl.codec.utf8String.toBits(adata);
-        }
-
-        prp = new sjcl.cipher[p.cipher](password);
-
-        /* return the json data */
-        j._add(p);
-
-        /* do the encryption */
-        ct = sjcl.mode[p.mode].encrypt(prp, plaintext, p.iv, adata, p.ts);
-        ct = sjcl.codec.bytes.fromBits(ct);
-        return {params: j.encode(p), ct: ct};
+    /* do the encryption */
+    ct = sjcl.mode[p.mode].encrypt(prp, plaintext, p.iv, adata, p.ts);
+    ct = sjcl.codec.bytes.fromBits(ct);
+    return {params: j.encode(p), ct: ct};
 };
 
 var decryptBinary = function (password, ciphertext, params) {
-    var j = sjcl.json,
-        p = params,
-        adata=p.adata,
-        ct,
-        tmp,
-        prp;
+    var j = sjcl.json, p = params, ct, tmp, prp, adata=p.adata;
 
     if (typeof p.salt === "string") {
         p.salt = sjcl.codec.base64.toBits(p.salt);
@@ -90,6 +75,7 @@ var decryptBinary = function (password, ciphertext, params) {
     if (typeof adata === "string") {
         adata = sjcl.codec.utf8String.toBits(adata);
     }
+
     prp = new sjcl.cipher[p.cipher](password);
 
     ciphertext = sjcl.codec.bytes.toBits(ciphertext);
@@ -100,10 +86,11 @@ var decryptBinary = function (password, ciphertext, params) {
 };
 
 var scrypt = function (passwd, options) {
-    return sjcl.misc.scrypt(passwd, options.salt, options.N, options.r, options.p, options.dkLen);
+  return sjcl.misc.scrypt(passwd, options.salt, options.N, options.r, options.p, options.dkLen);
 };
 
 self.addEventListener('message', function (ev) {
+
     var data = ev.data;
     switch (data.cmd) {
         case 'encryptBinary':
@@ -118,4 +105,5 @@ self.addEventListener('message', function (ev) {
         default:
             break;
     }
+
 }, false);
